b1eacfbefe0582e06ca7d35410d533eb1ce65971
nsohail <nsohail92@gmail.com>
2014-05-27 00:51:37 -0400

    Initital commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,93 @@
 
 $(document).ready(function(){
 	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+/*--- Display information modal box ---*/
+$(".what").click(function(){
+$(".overlay").fadeIn(1000);
 
 });
+
+ /*--- Hide information modal box ---*/
+ $("a.close").click(function(){
+ $(".overlay").fadeOut(1000);
+});
+
+
+ /*my code*/
+
+$('#guessButton').click(function(){
+playGame();
+event.preventDefault();
+});
+
+$('.new').click(function(){
+  newGame();
+});
+
+});//document ready ends here
+
+	
+	
+var random=Math.floor(Math.random()*100 + 1);
+
+
+
+function playGame(){
+
+	var guess=$('#userGuess').val();
+	var howClose=(Math.abs(random-guess));
+	
+	
+	if (guess >= 101 || guess <= 0 || isNaN(guess)){
+	alert('Guess a number between 1 and 100');
+	return false;
+	}
+
+
+
+	if ((howClose<=100) && (howClose>=50)){
+		
+		$('#feedback').text('You are ice cold');
+	}
+	if ((howClose<=49) && (howClose>=30)){
+		
+		$('#feedback').text('You are cold');
+	}
+	if ((howClose<=29) && (howClose>=10)){
+		
+		$('#feedback').text('Your are warm');
+	}
+	if ((howClose<=9) && (howClose>=1)){
+		$('#feedback').text('Your are hot');
+	}
+	if (howClose===0){
+		$('#feedback').text('You guessed it!');
+		$("#userGuess").prop("disabled",true );
+	}
+
+
+	$('#guessList').append('<li>' + guess); //same thing as <li>+guess+</li>
+
+
+	var countGuess=$('#guessList').children().length;
+	$('#count').text(countGuess);
+	
+	$('#userGuess').val("");
+
+}//end playGame function
+
+
+function newGame(){
+	$('#feedback').text('Make your Guess!');
+	$('#count').text('0');
+	$('#userGuess').val("");
+	$('#guessList').children('li').remove();
+	Math.floor(Math.random()*100 + 1);
+	$("#userGuess").prop("disabled",false );
+}
+
+
+
+
 
 
